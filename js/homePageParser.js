0a15297e06399256db14173fe9fc4afd67774397
sbaker2020 <sbaker2020@gmail.com>
2019-06-17 23:12:57 -0400

    updating parser

--- js/homePageParser.js
+++ js/homePageParser.js
@@ -1,7 +1,26 @@
 function loadJSONFromExcelFile(filePageLink) {
-	var workbook = XLSX.readFile(filePageLink);
-    var sheet_name_list = workbook.SheetNames;
-    console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+	var fileData = '';
+    $.ajax({
+        url: '../catering/' + eventId + '.txt',
+        type: "get",
+        async: false,
+        success: function(data) {
+            fileData = data;
+        },
+        error: function() {
+            console.log('Could not load event property data');
+        }
+    });
+	
+    var workbook = XLSX.read(fileData, {
+      type: 'binary'
+    });
+
+    workbook.SheetNames.forEach(function(sheetName) {
+      var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+      var json_object = JSON.stringify(XL_row_object);
+      console.log(json_object);
+    })
 }
 
 $( document ).ready(function() {
