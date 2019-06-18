81628b82933c952849099899c643af37d58c01f6
sbaker2020 <sbaker2020@gmail.com>
2019-06-17 21:33:15 -0400

    update xlsx parser

--- js/homePageParser.js
+++ js/homePageParser.js
@@ -1,23 +1,7 @@
 function loadJSONFromExcelFile(filePageLink) {
-	var fileData = '';
-    $.ajax({
-        url: filePageLink,
-        type: "get",
-        async: false,
-        success: function(data) {
-            fileData = data;
-        },
-        error: function() {
-            console.log('Could not load event property data');
-        }
-    });
-	
-	var workbook = XLSX.read(fileData, {type: 'binary'});
-	var firstSheet = workbook.SheetNames[0];
-	
-	var objectArray = XLSX.utils.sheet_to_row_object_array(firstSheet);
-	var json = JSON.stringify(objectArray);
-	console.log(json);
+	var workbook = XLSX.readFile(filePageLink);
+    var sheet_name_list = workbook.SheetNames;
+    console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
 }
 
 $( document ).ready(function() {
